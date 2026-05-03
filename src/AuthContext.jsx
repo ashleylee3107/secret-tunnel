1760b09ecf0cd0afa719ad8bdaf71d90605dc896
Ashley <ashleysofialee@gmail.com>
2026-05-03 15:25:27 -0500

    Finished all functions

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -5,14 +5,50 @@ const API = "https://fsa-jwt-practice.herokuapp.com";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState(null);
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup(username) {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+        }),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   // TODO: authenticate
+  async function authenticate() {
+    if (!token) {
+      throw Error("No token.");
+    }
+    try {
+      const response = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw Error("Authentication failed");
+      }
+      setLocation("TUNNEL");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-  const value = { location };
+  const value = { location, signup, token, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,16 @@
+import { useAuth } from "./AuthContext";
 /** Users can enter their name to receive a token from the API. */
 export default function Entrance() {
   // TODO: call signup when form is submitted
+  const { signup } = useAuth();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const username = formData.get("name");
+    signup(username);
+  }
 
   return (
     <>
@@ -15,12 +25,12 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name
           <input name="name" />
         </label>
-        <button>Respond</button>
+        <button type="submit">Respond</button>
       </form>
     </>
   );
